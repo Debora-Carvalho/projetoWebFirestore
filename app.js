2b1f489313fbbbf26ccf81a122d1f15690787af6
Debora-Carvalho <deboravolc2@gmail.com>
2024-10-11 10:40:46 -0300

    adicionar constantes

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+const handlebars = require('express-handlebars').engine
+const bodyParser = require('body-parser')
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 const serviceAccount = require('./projetodsw-140ae-firebase-adminsdk-u6rbe-30cbb9c1f5.json');
@@ -8,3 +12,17 @@ initializeApp({
   
 
 const db = getFirestore()
+
+app.engine('handlebars',handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req,res){
+    res.render('primeira_pagina')
+})
+
+app.listen(8081, function(){
+    console.log("Servidor ativo!")
+})
